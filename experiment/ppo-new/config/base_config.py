2f832c59a8b50a15d299c8f8ef70f51b18ed1c68
froitcake <111040659+froitcake@users.noreply.github.com>
2026-02-15 10:03:24 -0500

    Update base_config.py

--- experiment/ppo-new/config/base_config.py
+++ experiment/ppo-new/config/base_config.py
@@ -138,7 +138,8 @@ class BaseConfig:
 
 @dataclass
 class TestConfig(BaseConfig):
-    wandb: WandbConfig = field(default_factory=lambda: WandbConfig(en=False))    topk: int = 3
+    wandb: WandbConfig = field(default_factory=lambda: WandbConfig(en=False))    
+    topk: int = 3
     mode: str = 'test'
     resume: bool = True
     budget: int = int(1e8)
@@ -151,7 +152,8 @@ class TestConfig(BaseConfig):
 
 @dataclass
 class ConvertConfig(BaseConfig):
-    wandb: WandbConfig = field(default_factory=lambda: WandbConfig(en=False))    mode: str = 'convert'
+    wandb: WandbConfig = field(default_factory=lambda: WandbConfig(en=False))    
+    mode: str = 'convert'
     resume: bool = True
     ckpt_output_dir: str = '.'
     ckpt_output_path: str = ''
